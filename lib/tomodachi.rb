53f0e53dd435598d1697172a10c18e68279ead9f
Takashi Kokubun <takashikkbn@gmail.com>
2014-06-01 15:01:21 +0900

    Refactor tomodachi class

--- lib/tomodachi.rb
+++ lib/tomodachi.rb
@@ -9,26 +9,32 @@ class Tomodachi
   CONSUMER_KEY = 'IQKbtAYlXLripLGPWd0HUA'
   CONSUMER_SECRET = 'GgDYlkSvaPxGxC4X8liwpUoqKwwr3lCADbz8A7ADU'
 
-  def self.setup
+  def setup
     case ARGV[0]
     when 'auth'
-      auth = Tomodachi::Auth.new
       auth.create
     when 'list'
-      Tomodachi::Auth.list
+      auth.list
     when 'start'
       if ARGV[1]
         Tomodachi::Client.start(ARGV[1])
       else
-        puts 'Usage: tomodachi start [screen_name]'
+        puts <<-EOS.unindent
+          Usage:
+            tomodachi start [screen_name]
+        EOS
       end
     else
       puts <<-EOS.unindent
         Usage:
-          tomodachi auth  # add account
-          tomodachi list  # authenticated account list
-          tomodachi start # start automatic following back
+          tomodachi auth                # add account
+          tomodachi list                # authenticated account list
+          tomodachi start [screen_name] # start automatic following back
       EOS
     end
+  end
+
+  def auth
+    @auth ||= Tomodachi::Auth.new
   end
 end
